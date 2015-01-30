1e8aaf9d4070ad9a7a80afe992983c0edd6b1c0c
Shawn Kendrot <skendrot@sk-desktop.1rgs.com>
2015-01-30 11:17:38 -0700

    First commit for Refit.Zip This will allow Refit to use gzip/compression by default. Very useful for mobile apps

---
+++ Refit.Zip/RestService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+namespace Refit.Zip
+{
+    public static class RestService
+    {
+        public static T For<T>(string hostUrl, RefitSettings settings)
+        {
+            var handler = new HttpClientHandler();
+            if (handler.SupportsAutomaticDecompression)
+            {
+                handler.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+            }
+            var client = new HttpClient(handler) { BaseAddress = new Uri(hostUrl) };
+            return Refit.RestService.For<T>(client, settings);
+        }
+
+        public static T For<T>(string hostUrl)
+        {
+            return RestService.For<T>(hostUrl, null);
+        }
+    }
+}
